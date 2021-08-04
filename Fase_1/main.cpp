3bbdbf618b850020d5f5dc9118756e929f7651b6
Daniel Dubón <3179425811504@ingenieria.usac.edu.gt>
2021-08-03 19:54:01 -0600

    Initial commit

---
+++ Fase_1/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+using namespace std;
+
+int menu();
+void clear();
+void gotoxy(int, int);
+
+int main()
+{
+    int op;
+    do
+    {
+        op = menu();
+        switch (op)
+        {
+            case 0: break;
+            case 1: break;
+            case 2: break;
+            case 3: break;
+            case 4: break;
+        }
+    } while (op != 4);
+}
+
+int menu(void)
+{
+    char *m[5];
+    m[0] = "    1. Carga de usuarios";
+    m[1] = "    2. Carga de tareas";
+    m[2] = "    3. Ingreso manual";
+    m[3] = "    4. Reportes";
+    m[4] = "    5. Salir";
+    char lec;
+    int aux = 0, c, pos = 0;
+    while (aux != 13)
+    {
+        clear();
+        for (c = 0; c < 5; c++)
+        {
+            gotoxy(10, (c+5) * 2);
+            if (pos == c)
+            {
+                cout <<"--> ";
+            }
+            cout << m[c];
+        }
+
+        lec = getch();
+        aux = (int)lec;
+        if (aux == 72)
+        {
+            if (pos > 0)
+            {
+                pos = pos - 1;
+            }
+            else
+            {
+                pos = 4;
+            }
+        }
+        if (aux == 80)
+        {
+            if (pos < 4)
+            {
+                pos = pos + 1;
+            }
+            else
+            {
+                pos = 0;
+            }
+        }
+    }
+    return pos;
+}
+
+void clear()
+{
+    system("cls");
+}
+
+void gotoxy(int x, int y)
+{
+    HANDLE hCon;
+    hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD dwPos;
+    dwPos.X = x;
+    dwPos.Y = y;
+    SetConsoleCursorPosition(hCon, dwPos);
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# EDD_SmartClass_201901772
+ Proyecto por fases de la clase de Estructura de Datos
