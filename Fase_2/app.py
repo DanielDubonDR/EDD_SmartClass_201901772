3f5198b84a98c311a7cd09ec31cc15c214347031
Daniel Dubón <3179425811504@ingenieria.usac.edu.gt>
2021-09-21 22:36:58 -0600

    Carga de estudiantes al arbol AVL implementada

--- Fase_2/app.py
+++ Fase_2/app.py
@@ -1,19 +1,50 @@
 from flask import Flask, jsonify, request
+from Analizador.Syntactic import parser
+from Analizador.Syntactic import lista_usuarios, lista_tareas
+from Estructuras.AVL import AVL
 # from flask_cors import  CORS
 import json
 
-app = Flask(__name__)
+# *------------------------------------------------------ VARIABLES GLOBALES --------------------------------------------------------
+arbol_AVL = AVL()
 
+app = Flask(__name__)
+# *----------------------------------------------------------- FUNCIONES ------------------------------------------------------------
+def cargarUsuarios(path):
+    archivo = open(path,"r", encoding="utf-8")
+    contenido = archivo.read()
+    archivo.close()
+
+    parser.parse(contenido)
+    # print(lista_usuarios)
+    while lista_usuarios.isEmpy() != True:
+        nodeAux = lista_usuarios.pop()
+        arbol_AVL.add(nodeAux.carnet, nodeAux.dpi, nodeAux.nombre, nodeAux.carrera, nodeAux.password, nodeAux.creditos, nodeAux.edad, nodeAux.correo)
+
+def cargarTareas(path):
+    archivo = open(path,"r", encoding="utf-8")
+    contenido = archivo.read()
+    archivo.close()
+
+    parser.parse(contenido)
+    print(lista_tareas)
+# *-------------------------------------------------------------- API ---------------------------------------------------------------
 @app.route('/carga', methods=['POST'])
 def cargaMasiva():
     tipo = request.json['tipo']
     path = request.json['path']
     if tipo == "estudiante":
-        return jsonify({'Tipo': tipo})
+        cargarUsuarios(path)
+        arbol_AVL.graficar()
+        return jsonify({'Tipo': tipo, 'Mensaje': 'Se han cargado los estudiantes con exito'})
+
     elif tipo == "recordatorio":
-        return jsonify({'Tipo': tipo})
+        cargarTareas(path)
+        return jsonify({'Tipo': tipo, 'Mensaje': 'Se han cargado las tareas con exito'})
+
     elif  tipo == "curso":
         return jsonify({'Tipo': tipo})
+        
     else:
         return jsonify({'Error': 'Tipo invalido'})
 
